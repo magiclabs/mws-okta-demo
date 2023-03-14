ec331fc1e5e3e8e8344e00e90b0b8fcc18a38d58
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2023-03-14 15:33:24 -0700

    creates oktaConfig file

---
+++ config/oktaConfig.js
@@ -0,0 +1,16 @@
+const oktaConfig = {
+  oidc: {
+    issuer: process.env.OKTA_OAUTH2_ISSUER,
+    clientId: process.env.OKTA_OAUTH2_CLIENT_ID,
+    scopes: ["openid", "profile", "email"],
+    redirectUri: `${window.location.origin}/login/callback`,
+  },
+  widget: {
+    issuer: process.env.OKTA_OAUTH2_ISSUER,
+    clientId: process.env.OKTA_OAUTH2_CLIENT_ID,
+    scopes: ["openid", "profile", "email"],
+    redirectUri: `${window.location.origin}/login/callback`,
+  },
+};
+
+export default oktaConfig;
