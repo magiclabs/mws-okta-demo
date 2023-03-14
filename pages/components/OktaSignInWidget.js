d2d522a7e5288d27ebc3426d0993a9d009b4e191
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2023-03-14 15:57:22 -0700

    creates okta widget component

---
+++ pages/components/OktaSignInWidget.js
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from "react";
+import OktaSignIn from "@okta/okta-signin-widget";
+import "@okta/okta-signin-widget/dist/css/okta-sign-in.min.css";
+import oktaConfig from "@/config/oktaConfig";
+
+const OktaSignInWidget = ({ onSuccess, onError }) => {
+  const widgetRef = useRef();
+
+  useEffect(() => {
+    if (!widgetRef.current) {
+      return false;
+    }
+
+    const widget = new OktaSignIn(config.widget);
+
+    const searchParams = new URL(window.location.href).searchParams;
+    widget.otp = searchParams.get("otp");
+    widget.state = searchParams.get("state");
+    widget
+      .showSignInToGetTokens({
+        el: widgetRef.current,
+      })
+      .then(onSuccess)
+      .catch(onError);
+
+    return () => widget.remove();
+  }, [onSuccess, onError]);
+
+  return <div ref={widgetRef} />;
+};
+
+export default OktaSignInWidget;
